ad75796061dde72b8b969f716195078af8bf3dc4
15rudtp <thdrudtp15@naver.com>
2024-02-22 13:37:38 +0900

    데이터 조회 및 view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Post # .은 같은 경로에 있는 파일일 때 붙힌다. 
 
 # Create your views here.
 
 
 def post_list(request) : 
-    return render ( request , 'blog/post_list.html' , {})
+    posts = Post.objects.order_by('published_date') #정렬 쿼리셋을 이용한 데이터 조회
+    return render ( request , 'blog/post_list.html' , { 'posts' : posts })
